18f1f60e5d8bdf7a4b7c0706f8d51e3bb7441fbd
Oz the powerful <ozcodx++@gmail.com>
2024-11-15 16:55:52 -0500

    fix movies route

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const movieController = require('../controllers/movieController');
 
-router.get('/movies', movieController.getAllMovies);
+router.get('/', movieController.getAllMovies);
 
 module.exports = router;
